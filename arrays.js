cd887637d7e5db4a2a9a445d72db77040ee41ca0
Sergio Neves <serneves@hotmail.com>
2018-11-07 23:59:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ function addElementToBeginningOfArray (array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
-  return array.unshift(element)
+  return newArray = array.unshift(element)
 }
 
 function addElementToEndOfArray (array, element) {
