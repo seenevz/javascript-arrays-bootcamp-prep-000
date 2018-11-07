afc0691e79d5138ef0685414a7061b9d842986f1
Sergio Neves <serneves@hotmail.com>
2018-11-07 23:54:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ function addElementToBeginningOfArray (array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
-  return array.unshift(element);
+  return array.unshift(element)
 }
 
 function addElementToEndOfArray (array, element) {
@@ -14,7 +14,7 @@ function addElementToEndOfArray (array, element) {
 }
 
 function destructivelyAddElementToEndOfArray (array, element) {
-  return array.push(element);
+  return array.push(element)
 }
 
 function accessElementInArray (array, index) {
