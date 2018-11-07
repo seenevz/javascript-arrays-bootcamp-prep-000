e2e4d30d91a2f60ea8625a13941011f5f55acf67
Sergio Neves <serneves@hotmail.com>
2018-11-07 23:34:34 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,42 @@
+
+var chocolateBars=["snickers","hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray (array, element) {
+  return newArray = [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  return array.unshift(element)
+}
+
+function addElementToEndOfArray (array, element) {
+  return newArray = [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray (array, element) {
+  return array.push(element)
+}
+
+function accessElementInArray (array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+  var removed = array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray (array) {
+  return newArray = array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray (array) {
+  var removed = array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray (array) {
+  newArray = array.pop(
+    return array
+  )
+}
